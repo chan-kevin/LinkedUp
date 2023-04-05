f015a0b69b33454441d3b5953516cc1ae0d1f570
Kevin Chan <chankevin13@gmail.com>
2023-04-05 19:48:01 -0400

    edit

--- frontend/src/store/profile.js
+++ frontend/src/store/profile.js
@@ -9,21 +9,3 @@ const setUserProfile = (profile) => {
     };
 };
 
-export const fetchUserProfile = (userId) => async dispatch => {
-    const response = await csrfFetch(`/api/users/${userId}`);
-    const data = await response.json();
-    dispatch(setUserProfile(data));
-    return response;
-}
-
-const userReducer = (state = {}, action) => {
-    switch (action.type) {
-        case SET_USER_PROFILE:
-            debugger
-            return { ...state, ...action.payload.user };
-        default:
-            return state;
-    }
-};
-
-export default userReducer;
