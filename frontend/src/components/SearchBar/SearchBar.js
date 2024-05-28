2c5b220fa574766e0c67b141891d2484e7087df6
Kevin Chan <chankevin13@gmail.com>
2024-05-28 14:05:40 -0400

    edited style for searchbar

--- frontend/src/components/SearchBar/SearchBar.js
+++ frontend/src/components/SearchBar/SearchBar.js
@@ -1,42 +1,42 @@
-import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import * as searchActions from '../../store/search'
-import './SearchBar.css'
-import { useHistory } from 'react-router-dom';
-import { fetchRandomUsers } from '../../store/random';
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import * as searchActions from "../../store/search";
+import "./SearchBar.css";
+import { useHistory } from "react-router-dom";
+import { fetchRandomUsers } from "../../store/random";
 
 const SearchBar = () => {
-  const [query, setQuery] = useState('');
+  const [query, setQuery] = useState("");
   const [showModal, setShowModal] = useState(false);
   const dispatch = useDispatch();
-  const users = useSelector(state => Object.values(state.search));
-  const randomUsers = useSelector(state => Object.values(state.random));
+  const users = useSelector((state) => Object.values(state.search));
+  const randomUsers = useSelector((state) => Object.values(state.random));
   const history = useHistory();
   const [smallScreen, setSmallScreen] = useState(window.innerWidth <= 853);
   const [mobileSearch, setMobileSearch] = useState(false);
 
   const handleResize = () => {
     setSmallScreen(window.innerWidth <= 853);
-  }
+  };
 
   useEffect(() => {
-    window.addEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
 
     return () => {
-      window.removeEventListener('resize', handleResize);
-    }
+      window.removeEventListener("resize", handleResize);
+    };
   }, []);
 
-  const startSearch =() => {
+  const startSearch = () => {
     setShowModal(true);
     dispatch(fetchRandomUsers());
-  }
+  };
 
   const onClose = () => {
     setShowModal(false);
     setMobileSearch(false);
-    document.getElementById('searchBar').value='';
-  }
+    document.getElementById("searchBar").value = "";
+  };
 
   const searchUser = (e) => {
     setQuery(e.target.value);
@@ -45,80 +45,115 @@ const SearchBar = () => {
 
   const checkOutProfile = (userId) => {
     history.push(`/users/${userId}`);
-    window.scrollTo(0,0);
-    document.getElementById('searchBar').value='';
+    window.scrollTo(0, 0);
+    document.getElementById("searchBar").value = "";
     setShowModal(false);
-  }
+  };
 
   const openMobileSearch = () => {
     setMobileSearch(true);
     setShowModal(true);
-  }
+  };
 
-const matchedWord = (word) => {
+  const matchedWord = (word) => {
     if (word.toLowerCase().includes(query.toLowerCase())) {
-      return <div><span className='matched'>{word.slice(0, query.length)}</span><span>{word.slice(query.length, word.length)}</span></div>
+      return (
+        <div>
+          <span className="matched">{word.slice(0, query.length)}</span>
+          <span>{word.slice(query.length, word.length)}</span>
+        </div>
+      );
     } else {
-      return <div><p>{word}</p></div>
+      return (
+        <div>
+          <p>{word}</p>
+        </div>
+      );
     }
-}
+  };
 
   return (
-    <div className='fontFamily'>
-      {!smallScreen || (smallScreen && mobileSearch) || showModal? 
-      <div className='searchWithIcon'>
-          <i className="fa-solid fa-magnifying-glass" id='searchIcon'></i> 
-          <input type="text" onChange={searchUser} onClick={startSearch} placeholder='Search' id='searchBar'/>
-      </div>
-      : 
-      <label className='navButtons network' onClick={openMobileSearch}>
+    <div className="fontFamily">
+      {!smallScreen || (smallScreen && mobileSearch) || showModal ? (
+        <div className="searchWithIcon">
+          <i className="fa-solid fa-magnifying-glass" id="searchIcon"></i>
+          <input
+            type="text"
+            onChange={searchUser}
+            onClick={startSearch}
+            placeholder="Search"
+            id="searchBar"
+          />
+        </div>
+      ) : (
+        <label className="navButtons network" onClick={openMobileSearch}>
           <i className="fa-solid fa-magnifying-glass navlogo"></i>
-          <span className='navTitle navlink'>Search</span>
-      </label> }
+          <span className="navTitle navlink">Search</span>
+        </label>
+      )}
 
       {showModal && (
-        <div className="modal" id='searchModal'>
-        <div className="modal-background" id='searchModalBackground' onClick={onClose} />
-            <div className="modal-content" id='searchModalContent'>
-                {(query !== '') ? (
-                  <ul>
-                    {users.map((user, index) => (
-                      <li key={index} onClick={() => checkOutProfile(user.id)} className='searchResult'>
-                        <i className="fa-solid fa-magnifying-glass" id='insideSearch'></i>
-                        <div className='fullName'>
-                          {matchedWord(user.firstName)}
-                          <p>&nbsp;</p>
-                          {matchedWord(user.lastName)}
-                          <div className='headlineSearch'>&nbsp; &bull; &nbsp;{user.headline}</div>
-                        </div>
-                        <div className='authorPic' id='searchPhoto'>
-                          <img src={user.photoUrl} alt='defaultProfile' />
-                        </div>
-                      </li>
-                    ))}
-                  </ul>
-                ): (
-                  <ul>
-                    <li className='searchResult seachTitle'> Try Searching for</li>
+        <div className="modal" id="searchModal">
+          <div
+            className="modal-background"
+            id="searchModalBackground"
+            onClick={onClose}
+          />
+          <div className="modal-content" id="searchModalContent">
+            {query !== "" ? (
+              <ul>
+                {users.map((user, index) => (
+                  <li
+                    key={index}
+                    onClick={() => checkOutProfile(user.id)}
+                    className="searchResult"
+                  >
+                    <i
+                      className="fa-solid fa-magnifying-glass"
+                      id="insideSearch"
+                    ></i>
+                    <div className="fullName">
+                      {matchedWord(user.firstName)}
+                      <p>&nbsp;</p>
+                      {matchedWord(user.lastName)}
+                      <div className="headlineSearch">
+                        &nbsp; &bull; &nbsp;{user.headline}
+                      </div>
+                    </div>
+                    <div className="authorPic" id="searchPhoto">
+                      <img src={user.photoUrl} alt="defaultProfile" />
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <ul>
+                <li className="searchResult seachTitle"> Try Searching for</li>
 
-                    {randomUsers.map((user, index) => (
-                      <li key={index} onClick={() => checkOutProfile(user.id)} className='searchResult'>
-                        <i className="fa-solid fa-magnifying-glass" id='insideSearch'></i> 
-                            <div className='fullName'>
-                                <p>{user.firstName}</p>
+                {randomUsers.map((user, index) => (
+                  <li
+                    key={index}
+                    onClick={() => checkOutProfile(user.id)}
+                    className="searchResult"
+                  >
+                    <i
+                      className="fa-solid fa-magnifying-glass"
+                      id="insideSearch"
+                    ></i>
+                    <div className="fullName">
+                      <p>{user.firstName}</p>
 
-                                <p>&nbsp;</p>
+                      <p>&nbsp;</p>
 
-                                <p>{user.lastName}</p>
-                            </div>
-                        </li>
-                    ))}
-                  </ul>
-                )}
-            </div>
-      </div>
+                      <p>{user.lastName}</p>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            )}
+          </div>
+        </div>
       )}
-      
     </div>
   );
 };
