65042af3d2edf4052cd6aed12bb6db7a973fd927
Kevin Chan <chankevin13@gmail.com>
2024-06-11 17:45:59 -0400

    status loading

--- frontend/src/store/post.js
+++ frontend/src/store/post.js
@@ -1,95 +1,99 @@
 import csrfFetch from "./csrf";
+import { setLoading } from "./status";
 
-export const FETCH_ALL_POSTS = 'posts/fetchAllPosts'
-export const GET_POST = 'posts/getPost'
-const ADD_POST = 'posts/addPost'
-const EDIT_POST = 'posts/editPost'
-const DELETE_POST = 'posts/deletePost'
+export const FETCH_ALL_POSTS = "posts/fetchAllPosts";
+export const GET_POST = "posts/getPost";
+const ADD_POST = "posts/addPost";
+const EDIT_POST = "posts/editPost";
+const DELETE_POST = "posts/deletePost";
 
 export const fetchAllPosts = (posts) => {
-    return {
-      type: FETCH_ALL_POSTS,
-      payload: posts
-    };
+  return {
+    type: FETCH_ALL_POSTS,
+    payload: posts,
+  };
 };
 
 export const getPost = (post) => {
-    return {
-        type: GET_POST,
-        payload: post
-    }
-}
+  return {
+    type: GET_POST,
+    payload: post,
+  };
+};
 
 export const addPost = (post) => {
-    return {
-        type: ADD_POST,
-        payload: post
-    }
-}
+  return {
+    type: ADD_POST,
+    payload: post,
+  };
+};
 
 const editPost = (post) => {
-    return {
-        type: EDIT_POST,
-        payload: post
-    }
-}
+  return {
+    type: EDIT_POST,
+    payload: post,
+  };
+};
 
-export const createPost = (formData) => async dispatch => {
-    const response = await csrfFetch("/api/posts", {
+export const createPost = (formData) => async (dispatch) => {
+  const response = await csrfFetch("/api/posts", {
     method: "POST",
-    body: formData
-});
-    dispatch(addPost(formData));
-    return response;
+    body: formData,
+  });
+  dispatch(addPost(formData));
+  return response;
 };
 
-export const updatePost = (id, formData) => async dispatch => {
-    const response = await csrfFetch(`/api/posts/${id}`, {
+export const updatePost = (id, formData) => async (dispatch) => {
+  const response = await csrfFetch(`/api/posts/${id}`, {
     method: "PATCH",
-    body: formData
-});
-    dispatch(editPost(formData));
-    return response;
+    body: formData,
+  });
+  dispatch(editPost(formData));
+  return response;
 };
 
-export const getOnePost = (postId) => async dispatch => {
-    const response = await csrfFetch(`/api/posts/${postId}`)
-    const data = await response.json();
-    dispatch(getPost(data));
-    return response;
-}
+export const getOnePost = (postId) => async (dispatch) => {
+  const response = await csrfFetch(`/api/posts/${postId}`);
+  const data = await response.json();
+  dispatch(getPost(data));
+  return response;
+};
 
-export const getAllPosts = () => async dispatch => {
-    const response = await csrfFetch("/api/posts")
-    const data = await response.json();
-    dispatch(fetchAllPosts(data));
-    return response;
-}
+export const getAllPosts = () => async (dispatch) => {
+  dispatch(setLoading(true));
+  const response = await csrfFetch("/api/posts");
+  const data = await response.json();
+  dispatch(fetchAllPosts(data));
+  dispatch(setLoading(false));
+  return response;
+};
 
-export const removePost = (postId) => async dispatch => {
-    const response = await csrfFetch(`/api/posts/${postId}`, {
-        method: "DELETE"
-    });
-    const data = await response.json();
-    dispatch(fetchAllPosts(data));
-    return response;
+export const removePost = (postId) => async (dispatch) => {
+  const response = await csrfFetch(`/api/posts/${postId}`, {
+    method: "DELETE",
+  });
+  const data = await response.json();
+  dispatch(fetchAllPosts(data));
+  return response;
 };
 
 const postsReducer = (state = {}, action) => {
-    switch (action.type) {
-        case FETCH_ALL_POSTS:
-            return {...action.payload.posts}
-        case ADD_POST:
-            return { ...state, [action.payload.id]: action.payload }
-        case EDIT_POST:
-            return { ...state, [action.payload.id]: action.payload };
-        case DELETE_POST:
-            const newState = { ...state };
-            delete newState[action.payload];
-            return newState;
-        default:
-            return state;
-    }
-}
+  switch (action.type) {
+    case FETCH_ALL_POSTS:
+      console.log(state);
+      return { ...action.payload.posts };
+    case ADD_POST:
+      return { ...state, [action.payload.id]: action.payload };
+    case EDIT_POST:
+      return { ...state, [action.payload.id]: action.payload };
+    case DELETE_POST:
+      const newState = { ...state };
+      delete newState[action.payload];
+      return newState;
+    default:
+      return state;
+  }
+};
 
 export default postsReducer;
--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -1,68 +1,79 @@
 import csrfFetch from "./csrf";
+import { setLoading } from "./status";
 
-const SET_CURRENT_USER = 'session/setCurrentUser';
-const REMOVE_CURRENT_USER = 'session/removeCurrentUser';
+const SET_CURRENT_USER = "session/setCurrentUser";
+const REMOVE_CURRENT_USER = "session/removeCurrentUser";
 
 const setCurrentUser = (user) => {
   return {
     type: SET_CURRENT_USER,
-    payload: user
+    payload: user,
   };
 };
 
 const removeCurrentUser = () => {
   return {
-    type: REMOVE_CURRENT_USER
+    type: REMOVE_CURRENT_USER,
   };
 };
 
-const storeCSRFToken = response => {
-    const csrfToken = response.headers.get("X-CSRF-Token");
-    if (csrfToken) sessionStorage.setItem("X-CSRF-Token", csrfToken);
-}
-  
-const storeCurrentUser = user => {
-    if (user) sessionStorage.setItem("currentUser", JSON.stringify(user));
-    else sessionStorage.removeItem("currentUser");
-}
-  
-export const login = ({ credential, password }) => async dispatch => {
-  const response = await csrfFetch("/api/session", {
-    method: "POST",
-    body: JSON.stringify({ credential, password })
-  });
-  const data = await response.json();
-  storeCurrentUser(data.user);
-  dispatch(setCurrentUser(data.user));
-  return response;
+const storeCSRFToken = (response) => {
+  const csrfToken = response.headers.get("X-CSRF-Token");
+  if (csrfToken) sessionStorage.setItem("X-CSRF-Token", csrfToken);
 };
 
-export const signup = user => async dispatch =>{
+const storeCurrentUser = (user) => {
+  if (user) sessionStorage.setItem("currentUser", JSON.stringify(user));
+  else sessionStorage.removeItem("currentUser");
+};
+
+export const login =
+  ({ credential, password }) =>
+  async (dispatch) => {
+    dispatch(setLoading(true));
+
+    const response = await csrfFetch("/api/session", {
+      method: "POST",
+      body: JSON.stringify({ credential, password }),
+    });
+    const data = await response.json();
+    storeCurrentUser(data.user);
+    dispatch(setCurrentUser(data.user));
+    dispatch(setLoading(false));
+    return response;
+  };
+
+export const signup = (user) => async (dispatch) => {
   const { email, password, firstName, lastName, headline, location } = user;
   const response = await csrfFetch("/api/users", {
     method: "POST",
-    body: JSON.stringify({ 
-      user:{
-        email, password, firstName, lastName, headline, location
-      }
-     })
+    body: JSON.stringify({
+      user: {
+        email,
+        password,
+        firstName,
+        lastName,
+        headline,
+        location,
+      },
+    }),
   });
   const data = await response.json();
   storeCurrentUser(data.user);
   dispatch(setCurrentUser(data.user));
   return response;
-}
+};
 
 export const logout = () => async (dispatch) => {
   const response = await csrfFetch("/api/session", {
-    method: "DELETE"
+    method: "DELETE",
   });
   storeCurrentUser(null);
   dispatch(removeCurrentUser());
-  return response
+  return response;
 };
-  
-export const restoreSession = () => async dispatch => {
+
+export const restoreSession = () => async (dispatch) => {
   const response = await csrfFetch("/api/session");
   storeCSRFToken(response);
   const data = await response.json();
@@ -71,9 +82,9 @@ export const restoreSession = () => async dispatch => {
   return response;
 };
 
-const initialState = { 
-    user: JSON.parse(sessionStorage.getItem("currentUser"))
-  };
+const initialState = {
+  user: JSON.parse(sessionStorage.getItem("currentUser")),
+};
 
 const sessionReducer = (state = initialState, action) => {
   switch (action.type) {
